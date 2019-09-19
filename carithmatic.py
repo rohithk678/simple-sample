cbffccfc42e7130d116adae11033b93291eb6109
Rohith K <rohith.k678@gmail.com>
2019-09-19 09:45:13 +0530

    Initial commit

---
+++ carithmatic.py
@@ -0,0 +1,17 @@
+"""
+Created on Thu May  2 19:16:00 2019
+
+@author: rohith
+"""
+
+a = 12
+b = 3
+print(a+b/3-4*12)
+print(8/2*3)
+print(8*3/2)
+print((((a+b)/3)-4)*12)
+c=a+b
+d=c/3
+e=d-4
+f=e*12
+print(f)
---
+++ cgreeting.py
@@ -0,0 +1,6 @@
+greeting = "Bruce"
+_myName = "Rohith"
+Rohith67 = "Good"
+Rohith_was_45 = "Hello"
+Greeting = "There"
+print(Rohith_was_45 + ' ' + greeting)
